f2e9ba220d02de8e6750de1ace8319f7f66e3a9c
ladiespls <fsta657@aucklanduni.ac.nz>
2018-01-25 23:20:06 +1300

    Init code, saving for tomoz!

---
+++ parseCSV.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+
+var workflows = {};
+fs.createReadStream('./sample.csv')
+  .pipe(parse({delimiter: ':'}))
+  .on('data', function (row) {
+    var values = row.toString().split(',');
+    if (!workflows[values[3]]) {
+      workflows[values[3]] = {
+        states: {}
+      };
+    }
+    if (!workflows[values[3]].states[values[0]]) {
+      workflows[values[3]].states[values[0]] = {
+        initial: values[1] === 'Y',
+        final: values[2] === 'Y',
+        actions: {}
+      };
+    }
+    if (values[4] !== 'NULL') {
+      if (!workflows[values[3]].states[values[0]].actions[values[4]]) {
+        workflows[values[3]].states[values[0]].actions[values[4]] = [];
+      }
+      workflows[values[3]].states[values[0]].actions[values[4]].push(values[5]);
+    }
+  })
+  .on('end', function () {
+    fs.writeFile('workflows.json', 'var workflows = `' + JSON.stringify(workflows) + '`', 'utf8', function () {});
+  });
---
+++ sample.js
@@ -0,0 +1,57 @@
+var workflowInfo = JSON.parse(workflows);
+console.log(workflowInfo);
+
+// Create a new directed graph
+var g = new dagreD3.graphlib.Graph({compound: true}).setGraph({});
+
+// States and transitions
+for (var key in workflowInfo) {
+  var thisWorkflow = workflowInfo[key].states;
+  g.setNode(key, { label: key, clusterLabelPos: 'top', style: 'fill: #d3d3d3' });
+  for (var state in thisWorkflow) {
+    // Automatically label each of the nodes
+    g.setNode(key + '_' + state, { label: state, style: thisWorkflow[state].final ? 'fill: 	#ffb6c1' : (thisWorkflow[state].initial ? 'fill: #98fb98' : 'fill: #f5f5f5')});
+    g.setParent(key + '_' + state, key);
+  }
+
+  for (var stateNodes in thisWorkflow) {
+    for (var action in thisWorkflow[stateNodes].actions) {
+      for (var i = 0; i < thisWorkflow[stateNodes].actions[action].length; i++) {
+        g.setEdge(key + '_' + stateNodes, key + '_' + thisWorkflow[stateNodes].actions[action][i], { label: action });
+      }
+    }
+  }
+}
+
+// Set some general styles
+g.nodes().forEach(function (v) {
+  var node = g.node(v);
+  node.rx = node.ry = 5;
+});
+
+/*
+// Add some custom colors based on state
+g.node('CLOSED').style = 'fill: #f77';
+g.node('ESTAB').style = 'fill: #7f7';
+*/
+
+var svg = d3.select('svg');
+var inner = svg.select('g');
+
+// Set up zoom support
+var zoom = d3.zoom().on('zoom', function () {
+  inner.attr('transform', d3.event.transform);
+});
+svg.call(zoom);
+
+// Create the renderer
+var render = new dagreD3.render();
+
+// Run the renderer. This is what draws the final graph.
+render(inner, g);
+
+// Center the graph
+var initialScale = 0.75;
+svg.call(zoom.transform, d3.zoomIdentity.translate((svg.attr('width') - g.graph().width * initialScale) / 2, 20).scale(initialScale));
+
+svg.attr('height', g.graph().height * initialScale + 40);
